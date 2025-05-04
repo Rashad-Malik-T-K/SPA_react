c52d30335854bd4d76d5cb34c1aa75032058981d
Rashad Malik <rashad.malik@iic.ac.in>
2025-05-05 01:22:22 +0530

    verify change

--- src/pages/StudentDashboard.js
+++ src/pages/StudentDashboard.js
@@ -14,7 +14,13 @@ const StudentDashboard = () => {
       <h1 className="dashboard-title">Student Dashboard</h1>
       {/* <p className="dashboard-description">View your performance and insights below:</p> */}
       <div className="dashboard-iframe-container">
-      <iframe width="1250" height="600" src="https://lookerstudio.google.com/embed/reporting/1a6441d8-d592-420a-835a-d886f4f2405a/page/WIrIF" frameborder="0" ></iframe>
+        <iframe
+          title="Student Performance Dashboard"
+          width="1250"
+          height="600"
+          src="https://lookerstudio.google.com/embed/reporting/1a6441d8-d592-420a-835a-d886f4f2405a/page/WIrIF"
+          frameBorder="0"
+        ></iframe>
       </div>
       <button className="back-button" onClick={handleBack}>Back to Dashboard</button>
     </div>
